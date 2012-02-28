71a7590d399cf9c21ffdb98c10079640ad45bb5c
Guille <guille@ggalmazor.com>
2012-02-28 22:33:42 +0100

    Code styled and imports optimized

--- src/main/groovy/com.groomple/Groomple.groovy
+++ src/main/groovy/com.groomple/Groomple.groovy
@@ -1,65 +1,66 @@
 package com.groomple
 
-import com.groomple.service.*
+import com.groomple.service.ProtectedService
+import com.groomple.service.SharedService
 
 class Groomple implements Iterable {
-	def services = [:]
+  def services = [:]
 
-	Iterator iterator() {
-		return services.iterator()
-	}
+  Iterator iterator() {
+    return services.iterator()
+  }
 
-	def getService(String name) {
-		if (!services.containsKey(name))
-			throw new UnknownServiceException("Service ${name} is not defined")
-		return isCallable(services[name]) ? services[name]() : services[name]
-	}
+  def getService(String name) {
+    if (!services.containsKey(name))
+      throw new UnknownServiceException("Service ${name} is not defined")
+    return isCallable(services[name]) ? services[name]() : services[name]
+  }
 
-	// Groovy needs this method to be able to manage
-	// services as if they were object fields
-	// (aka return container.service)
-	def getProperty(String name) {
-		getService name
-	}
+  // Groovy needs this method to be able to manage
+  // services as if they were object fields
+  // (aka return container.service)
+  def getProperty(String name) {
+    getService name
+  }
 
-	void setService(String name, value) {
-		services[name] = value
-	}
+  void setService(String name, value) {
+    services[name] = value
+  }
 
-	// Groovy needs this method to be able to manage
-	// services as if they were object fields
-	// (aka container.service = something)
-	void setProperty(String name, value) {
-		setService name, value
-	}
+  // Groovy needs this method to be able to manage
+  // services as if they were object fields
+  // (aka container.service = something)
+  void setProperty(String name, value) {
+    setService name, value
+  }
 
-	void leftShift(serviceCollection) {
-		putAll(serviceCollection)
-	}
+  void leftShift(serviceCollection) {
+    putAll(serviceCollection)
+  }
 
-	void putAll(serviceCollection) {
-		serviceCollection.each {
-			setService it.key, it.value
-		}
-	}
+  void putAll(serviceCollection) {
+    serviceCollection.each {
+      setService it.key, it.value
+    }
+  }
 
-	def invokeMethod(String name, args) {
-		return getService(name)
-	}
-	
-	void remove(String service) {
-		services.remove service
-	}
+  def invokeMethod(String name, args) {
+    return getService(name)
+  }
 
-	void share(String service, Closure callable) {
-		setProperty(service, new SharedService(callable))
-	}
+  void remove(String service) {
+    services.remove service
+  }
 
-	void protect(String service, Closure callable) {
-		setProperty(service, new ProtectedService(callable))
-	}
+  void share(String service, Closure callable) {
+    setProperty(service, new SharedService(callable))
+  }
 
-	private isCallable(Object object) {
-		return object instanceof Closure || object instanceof SharedService || object instanceof ProtectedService
-	}
+  void protect(String service, Closure callable) {
+    setProperty(service, new ProtectedService(callable))
+  }
+
+  private isCallable(Object object) {
+    return object instanceof Closure || object instanceof SharedService || object instanceof ProtectedService
+  }
 }
